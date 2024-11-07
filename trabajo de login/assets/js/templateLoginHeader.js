a9c28ea6261b238232915514ac553341f436122e
Crizzi-11 <cristoferm4r1npro@outlook.com>
2024-11-07 14:51:17 -0600

    commit de Logins y Registers
    se subirá la practicas de logins y Register

---
+++ trabajo de login/assets/js/templateLoginHeader.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+header.innerHTML =`
+<div class=" navbarLogin">
+    <div class="Intel">
+      <nav>
+        <h2 class="Logo"> Intel <span>Corporation</span></h2>
+        <ul>
+          <li><a href="#">Mision</a></li>
+          <li><a href="#">Vison</a></li>
+          <li><a href="#">Contactanos</a></li>
+        </ul>
+        <button class="BtnNav">Registrarse</button>
+      </nav>
+    </div>
+  </div>
+`;
